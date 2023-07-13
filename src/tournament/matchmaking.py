050a1d57aa5b834854e26f59c36611077dbaf7c2
Nobel Edgar <nobel.edgar@mastersystem.co.id>
2023-07-13 11:12:04 +0700

    minor changes

--- src/tournament/matchmaking.py
+++ src/tournament/matchmaking.py
@@ -19,3 +19,20 @@ def start_round(id=None):
 @app.delete('/tournament/<id>/match')
 def reset(id=None):
     return id
+
+# endpoint /user/<id>/match/{id}
+@app.get('/tournament/<id>/match')
+def show_matchmaking(id=None):
+    return id + "match"
+
+@app.post('/tournament/<id>/match')
+def update_score(id=None):
+    return id
+
+@app.put('/tournament/<id>/match')
+def start_round(id=None):
+    return id
+
+@app.delete('/tournament/<id>/match')
+def reset(id=None):
+    return id
